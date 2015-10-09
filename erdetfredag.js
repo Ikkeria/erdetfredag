994e45ec2dbced6d5decacbc8cbead0c60214cf1
Ikkeria <ikkenoget@hotmail.com>
2015-10-09 11:45:07 +0200

    Update erdetfredag.js

--- erdetfredag.js
+++ erdetfredag.js
@@ -2,11 +2,11 @@
 // @name           er det fredag
 // @version        1.0
 // @namespace      
+// @grant          none
 // @author         RA
 // @description    Every day is champagn day at cloud-valley
 // @include         http://www.erdetfredag.dk/*
 // ==/UserScript==
-
 
 window.addEventListener('load', function() {
 
